766e35091997ac59726ee92882d21620d7c8aca5
Lukmandst <lukmandwi11@gmail.com>
2022-05-12 21:09:02 +0700

    auth check token

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const { errorResponseDefault } = require("../helpers/response");
 const { getUserByEmail } = require("../models/auth");
 
@@ -5,7 +6,9 @@ const checkDuplicate = (req, res, next) => {
   getUserByEmail(req.body.email)
     .then((result) => {
       if (result.rowCount > 0)
-        return errorResponseDefault(res, 400, { msg: "Email is already registered" });
+        return errorResponseDefault(res, 400, {
+          msg: "Email is already registered",
+        });
       next();
     })
     .catch((error) => {
@@ -14,4 +17,26 @@ const checkDuplicate = (req, res, next) => {
     });
 };
 
-module.exports = {checkDuplicate};
+const checkToken = (req, res, next) => {
+  const bearerToken = req.header("Authorization");
+  if (!bearerToken) {
+    return errorResponseDefault(res, 401, { msg: "Please Sign In first!" });
+  }
+  const token = bearerToken.split(" ")[1];
+  jwt.verify(
+    token,
+    process.env.JWT_SECRET,
+    { issuer: process.env.JWT_ISSUER },
+    (err, payload) => {
+      if (err && err.name === "TokenExpiredError")
+        return errorResponseDefault(res, 401, {
+          msg: "Your session was expired, please Sign In again!",
+        });
+      req.userPayload = payload;
+      console.log(payload);
+      next();
+    }
+  );
+};
+
+module.exports = { checkDuplicate, checkToken };
