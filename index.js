d777e1f1985c09b0bcc765688264015a6bc877e7
Lukmandst <lukmandwi11@gmail.com>
2022-05-16 21:58:43 +0700

    add multipart form data

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require("express"); // import package express
 const mainRouter = require("./src/routes");
 const db = require("./src/config/database");
 const logger = require("morgan");
+const multer = require("multer");
+const upload = multer();
 // create express application
 const server = express();
 const PORT = 8080;
@@ -14,8 +16,9 @@ db.connect()
     server.use(
       logger(":method :url :status :res[content-length] - :response-time ms")
     );
-    server.use(express.urlencoded({ extended: false }));
-    server.use(express.json());
+    server.use(upload.array()); // multipart/form-data
+    server.use(express.urlencoded({ extended: false })); // urlencoded
+    server.use(express.json()); // application/json
     server.use(mainRouter);
     server.use(express.static("public"));
 
